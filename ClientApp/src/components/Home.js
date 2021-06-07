a17ca2a04d884a49c5c0e7be2be528aec9a84b91
Daniel <[daniel23xu@hotmail.com]>
2021-06-08 01:55:46 +0200

    Initial

---
+++ ClientApp/src/components/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Home.css';
+
+export class Home extends Component {
+  static displayName = Home.name;
+
+  render () {
+    return (
+        <div className="home" >
+
+      
+        </div>
+    );
+  }
+}
