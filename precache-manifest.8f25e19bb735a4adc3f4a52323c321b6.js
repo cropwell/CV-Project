8ae1a904f1a1b966ab533b0e040cce5e6a3b2975
Daniel <[daniel23xu@hotmail.com]>
2021-06-08 14:24:10 +0200

    Updates

---
+++ precache-manifest.8f25e19bb735a4adc3f4a52323c321b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3d7f349b302a3c6432fcbf8122e7a9e",
+    "url": "/Cv-Project/index.html"
+  },
+  {
+    "revision": "2b306f033202b4879f68",
+    "url": "/Cv-Project/static/css/2.87a9c7a2.chunk.css"
+  },
+  {
+    "revision": "beed001ec843ed147df5",
+    "url": "/Cv-Project/static/css/main.5b5b4dae.chunk.css"
+  },
+  {
+    "revision": "2b306f033202b4879f68",
+    "url": "/Cv-Project/static/js/2.fd11d970.chunk.js"
+  },
+  {
+    "revision": "7e8409f8cb3821d4e1cacd0d340671a3",
+    "url": "/Cv-Project/static/js/2.fd11d970.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "beed001ec843ed147df5",
+    "url": "/Cv-Project/static/js/main.035c4b56.chunk.js"
+  },
+  {
+    "revision": "105d7cf6425919b4f32f",
+    "url": "/Cv-Project/static/js/runtime-main.fa857980.js"
+  }
+]);
